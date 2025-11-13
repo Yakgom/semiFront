d308f3a6894966704847636ac25b76816ae8f12a
이름 <>
2025-11-13 14:39:52 +0900

    header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./component/Common/Footer/Footer";
+import Header from "./component/Common/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <Header />
       <Routes>
         <Route path="/" />
       </Routes>
