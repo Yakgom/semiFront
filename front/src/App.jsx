938a713616782d4bf57b1f51a300575990fde7f1
yakgom <dirrha123@naver.com>
2025-11-14 17:04:17 +0900

    commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,21 +4,21 @@ import Footer from "./component/Common/Footer/Footer";
 import Header from "./component/Common/Header/Header";
 import Home from "./component/Common/Home/Home";
 import AdminHome from "./Admin/Pages/AdminHome";
+import Join from "./component/Member/Join/Join";
 
 function App() {
   const location = useLocation();
   const isAdminPage = location.pathname.startsWith("/admin");
-
+  const isJoin = location.pathname.startsWith("/members/join");
   return (
     <>
-      {!isAdminPage && <Header />}
-
+      {!isJoin && !isAdminPage && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin/*" element={<AdminHome />} />
+        <Route path="/members/join" element={<Join />} />
       </Routes>
-
-      {!isAdminPage && <Footer />}
+      {!isJoin && !isAdminPage && <Footer />}
     </>
   );
 }
